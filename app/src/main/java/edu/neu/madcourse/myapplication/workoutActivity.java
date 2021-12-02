e65b40e87b3e158231ba1ac07ce98c2b489aa91c
Toan <tvang994@gmail.com>
2021-12-01 20:53:26 -0800

    picker with diaglog

--- app/src/main/java/edu/neu/madcourse/myapplication/workoutActivity.java
+++ app/src/main/java/edu/neu/madcourse/myapplication/workoutActivity.java
@@ -2,7 +2,6 @@ package edu.neu.madcourse.myapplication;
 
 import android.app.Dialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
@@ -13,39 +12,53 @@ import androidx.appcompat.app.AppCompatActivity;
 public class workoutActivity extends AppCompatActivity {
     private NumberPicker exerciseTimePicker;
     private TextView exerciseTime;
+    static Dialog d ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
-
-        exerciseTimePicker = findViewById(R.id.exercisetimepicker);
         exerciseTime = findViewById(R.id.exercisetime);
+
+        exerciseTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show();
+            }
+        });
+
+
+}
+    public void show()
+    {
+        final Dialog d = new Dialog(this);
+        d.setTitle("NumberPicker");
+        d.setContentView(R.layout.dialog);
+        Button b2 = d.findViewById(R.id.ok);
+        exerciseTimePicker = d.findViewById(R.id.exercisetimepicker);
 
         Time.initTime();
         exerciseTimePicker.setMaxValue(Time.getTimeArrayList().size() -1);
         exerciseTimePicker.setMinValue(0);
         exerciseTimePicker.setDisplayedValues(Time.timeString());
 
-        exerciseTimePicker.setVisibility(View.GONE);
-
-        exerciseTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                exerciseTimePicker.setVisibility(View.VISIBLE);
-//                show();
-            }
-        });
-
-
-
         exerciseTimePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker numberPicker, int oldValue, int newValue) {
                 exerciseTime.setText(Time.getTimeArrayList().get(newValue).getMinute() +":"+ Time.getTimeArrayList().get(newValue).getSecond());
             }
-
     });
-}
+
+        b2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                d.dismiss();
+            }
+        });
+        d.show();
+    }
+
 
 }
