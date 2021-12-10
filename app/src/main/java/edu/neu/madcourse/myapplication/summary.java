f2122a22ffaddd85f6e4cfc9b546ef3c8423db28
je-cao <cao.zhix@northeastern.edu>
2021-12-09 22:06:04 -0800

    connect to firebase

--- app/src/main/java/edu/neu/madcourse/myapplication/summary.java
+++ app/src/main/java/edu/neu/madcourse/myapplication/summary.java
@@ -1,28 +1,49 @@
 package edu.neu.madcourse.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class summary extends AppCompatActivity {
-    private String exe;
-    private String rest;
-    private String totalET;
-    private String totalRT;
-    TextView totalresttimesummary, totalexercisetimesummary;
+    TextView exe, rest, conditionTextView;
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference conditionRef = rootRef.child("exerciseTime");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
-        totalresttimesummary = findViewById(R.id.totalresttimesummary);
-        totalexercisetimesummary = findViewById(R.id.totalexercisetimesummary);
-        Intent data = getIntent();
-        totalET = data.getStringExtra("totalET");
-        totalRT = data.getStringExtra("totalRT");
-        totalresttimesummary.setText(totalRT);
-        totalexercisetimesummary.setText(totalET);
+        rest = findViewById(R.id.rest);
+        exe = findViewById(R.id.exe);
+        conditionTextView = findViewById(R.id.update);
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        conditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String text = snapshot.getValue(String.class);
+                conditionTextView.setText(text);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
